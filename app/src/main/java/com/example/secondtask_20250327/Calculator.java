3d0bf75ae1bd771cd4ee29bb489a9b6816f3daa2
Martynas <martynas.bartnykas@gmail.com>
2025-04-07 22:19:46 +0300

    created unit tests, other minor fixes

--- app/src/main/java/com/example/secondtask_20250327/Calculator.java
+++ app/src/main/java/com/example/secondtask_20250327/Calculator.java
@@ -4,18 +4,18 @@ package com.example.secondtask_20250327;
 public class Calculator {
     public static int countWords(String text){
         //checking if the text is empty
-        if (text.isEmpty()){
+        if (text == null || text.trim().isEmpty()){
             return 0;
         }
         //splitting the text into an array of words based on whitespace
         //"\\s+" is a regular expression that matches any sequence of one or more whitespace characters
-        String[] words = text.split("\\s+");
+        String[] words = text.trim().split("\\s+");
         return words.length; //return the length of words in array using length property
     }
 
     public static int countCharacters(String text){
         //checking if the text is empty
-        if (text.isEmpty()){
+        if (text == null || text.isEmpty()){
             return 0;
         }
         return text.length(); //return the length of text characters using built-in length method.
--- app/src/test/java/com/example/secondtask_20250327/ExampleUnitTest.java
+++ app/src/test/java/com/example/secondtask_20250327/ExampleUnitTest.java
@@ -11,7 +11,110 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void empty_String_When_CountWords_Then_Return_Zero() {
+        String givenString = "";
+
+        int expectedRes = 0;
+        int actualRes = Calculator.countWords(givenString);
+        assertEquals(expectedRes, actualRes);
+    }
+
+    @Test
+    public void null_String_When_CountWords_Then_Return_Zero() {
+        String givenString = null;
+
+        int expectedRes = 0;
+        int actualRes = Calculator.countWords(givenString);
+        assertEquals(expectedRes, actualRes);
+    }
+
+    @Test
+    public void input_OneWord_When_CountWords_Then_Return_One() {
+        String givenString = "Hello";
+
+        int expectedRes = 1;
+        int actualRes = Calculator.countWords(givenString);
+        assertEquals(expectedRes, actualRes);
+    }
+
+    @Test
+    public void input_MultipleWords_When_CountWords_Then_Return_CorrectCount() {
+        String givenString = "Laba diena su vistiena";
+
+        int expectedRes = 4;
+        int actualRes = Calculator.countWords(givenString);
+        assertEquals(expectedRes, actualRes);
+    }
+
+    @Test
+    public void input_SymbolsOnly_When_CountWords_Then_Return_Zero() {
+        String givenString = "!@#$%^&*()_+";
+
+        int expectedRes = 0;
+        int actualRes = Calculator.countWords(givenString);
+        assertEquals(expectedRes, actualRes);
+    }
+
+    @Test
+    public void whitespace_Only_When_CountWords_Then_Return_Zero() {
+        String givenString = "     ";
+
+        int expectedRes = 0;
+        int actualRes = Calculator.countWords(givenString);
+        assertEquals(expectedRes, actualRes);
+    }
+
+    @Test
+    public void empty_String_When_CountCharacters_Then_Return_Zero() {
+        String givenString = "";
+
+        int expectedRes = 0;
+        int actualRes = Calculator.countCharacters(givenString);
+        assertEquals(expectedRes, actualRes);
+    }
+
+    @Test
+    public void null_String_When_CountCharacters_Then_Return_Zero() {
+        String givenString = null;
+
+        int expectedRes = 0;
+        int actualRes = Calculator.countCharacters(givenString);
+        assertEquals(expectedRes, actualRes);
+    }
+
+    @Test
+    public void input_OneWord_When_CountCharacters_Then_Return_CorrectCount() {
+        String givenString = "Nebeprisikiskiakopusteliaujantiesiems";
+
+        int expectedRes = 37;
+        int actualRes = Calculator.countCharacters(givenString);
+        assertEquals(expectedRes, actualRes);
+    }
+
+    @Test
+    public void input_MultipleWords_When_CountCharacters_Then_Return_CorrectCount() {
+        String givenString = "Laba diena su vistiena";
+
+        int expectedRes = 22;
+        int actualRes = Calculator.countCharacters(givenString);
+        assertEquals(expectedRes, actualRes);
+    }
+
+    @Test
+    public void input_SymbolsOnly_When_CountCharacters_Then_Return_CorrectCount() {
+        String givenString = "!@#*&^*&(*^^)#$%^&*()_+";
+
+        int expectedRes = 24;
+        int actualRes = Calculator.countCharacters(givenString);
+        assertEquals(expectedRes, actualRes);
+    }
+
+    @Test
+    public void whitespace_Only_When_CountWords_Then_Return_CorrectCount() {
+        String givenString = "          ";
+
+        int expectedRes = 10;
+        int actualRes = Calculator.countCharacters(givenString);
+        assertEquals(expectedRes, actualRes);
     }
 }
